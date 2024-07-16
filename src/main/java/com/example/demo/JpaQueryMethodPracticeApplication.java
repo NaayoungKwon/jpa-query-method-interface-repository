c57ce075961256d62d4acd9f11fc8c1cdc7d73b7
naayoungkwon <kny8092@gmail.com>
2024-07-16 22:06:42 +0900

    init

---
+++ src/main/java/com/example/demo/JpaQueryMethodPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueryMethodPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQueryMethodPracticeApplication.class, args);
+	}
+
+}
