7f97528bda912e26f5ad50d21104732003dad03f
naayoungkwon <kny8092@gmail.com>
2024-07-19 18:49:56 +0900

    [bug] mock parser return 변경

--- src/test/java/com/example/demo/common/parser/FakeParser.java
+++ src/test/java/com/example/demo/common/parser/FakeParser.java
@@ -5,6 +5,6 @@ public class FakeParser implements Parser {
 
   @Override
   public String parse(String methodName, String tableName, Class<?>[] parameterTypes) {
-    return "select * from user where name = ? and email = ?";
+    return "select * from user where username = %s";
   }
 }
